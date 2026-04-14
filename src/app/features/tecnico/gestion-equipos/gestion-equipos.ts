72cabc6f34a266733b3799451ad619f8d9ab4c2a
Angelzyzz7 <luisgignac4@gmail.com>
2026-04-13 23:06:01 -0600

    feat: conexion backend equipos y tabla dinamica en gestion de equipos

--- src/app/features/tecnico/gestion-equipos/gestion-equipos.ts
+++ src/app/features/tecnico/gestion-equipos/gestion-equipos.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { EquiposService, Equipo } from '../services/equipos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gestion-equipos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './gestion-equipos.html',
   styleUrl: './gestion-equipos.scss',
 })
 export class GestionEquipos {
 
+  equipos: Equipo[] = [];
+
+constructor(private equiposService: EquiposService) {}
+
+ngOnInit() {
+  this.cargarEquipos();
 }
+
+cargarEquipos() {
+  this.equiposService.getEquipos().subscribe(data => {
+    this.equipos = data;
+  });
+}
+
+eliminar(id: number) {
+  this.equiposService.eliminarEquipo(id).subscribe(() => {
+    this.cargarEquipos();
+  });
+}
+
+}
+
