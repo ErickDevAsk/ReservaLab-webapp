df23a084e3f75d605071312955c84e95594f56b2
ErickDevAsk <112228747+ErickDevAsk@users.noreply.github.com>
2026-05-05 14:56:35 -0600

    Feat: Datos Profile admin cargados

--- src/app/features/admin/admin-profile/admin-profile.ts
+++ src/app/features/admin/admin-profile/admin-profile.ts
@@ -1,27 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
-  selector: 'app-admin-profile',
+  selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './admin-profile.html',
   styleUrls: ['./admin-profile.scss']
 })
-export class AdminProfileComponent {
-  // Datos del Administrador
-  admin = {
-    nombre: 'Oscar Tomas',
-    rol: 'Administrador Global',
-    correo: 'oscar.tomas@alumno.buap.mx',
-    departamento: 'Gestión de Laboratorios FCC'
+export class AdminProfileComponent implements OnInit {
+  private readonly perfilService = inject(AuthService);
+
+  // Usamos un signal iniciando en null para saber cuando está cargando
+  public admin = signal<any>(null);
+
+  // Stats simuladas por ahora (puedes conectarlas a otro endpoint después)
+  public stats = {
+    usuariosTotales: 120,
+    tecnicosActivos: 8,
+    laboratorios: 5,
+    reservasHoy: 34
   };
 
-  // Estadísticas Globales del Sistema
-  stats = {
-    usuariosTotales: 142,
-    tecnicosActivos: 8,
-    laboratorios: 12,
-    reservasHoy: 24
-  };
+  ngOnInit(): void {
+    this.cargarPerfil();
+  }
+
+  cargarPerfil(): void {
+    this.perfilService.obtenerPerfil().subscribe({
+      next: (data: any) => {
+        // Guardamos los datos reales de Django en el signal
+        this.admin.set(data);
+      },
+      error: (err: any) => {
+        console.error('Error al cargar el perfil', err);
+      }
+    });
+  }
 }
